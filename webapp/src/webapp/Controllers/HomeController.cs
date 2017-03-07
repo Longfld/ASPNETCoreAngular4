3142e05327d7f1ca861a1a75bd5d5ecaf0b32c6a
Longfld <Longfld@users.noreply.github.com>
2017-03-07 16:13:51 +1100

    Add serilog logging

--- webapp/src/webapp/Controllers/HomeController.cs
+++ webapp/src/webapp/Controllers/HomeController.cs
@@ -4,9 +4,21 @@ namespace webapp.Controllers
 {
     public class HomeController : Controller
     {
+         public HomeController(IHostingEnvironment hostingEnvironment, ILogger<HomeController> logger)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            _logger = logger;
+           
+        }
+       
+        
         public IActionResult Index()
         {
+          _logger.LogError("HomeController: Helloworld from Index");
             return View();
         }
+        
+         private readonly IHostingEnvironment _hostingEnvironment;
+        private readonly ILogger<HomeController> _logger;
     }
 }
